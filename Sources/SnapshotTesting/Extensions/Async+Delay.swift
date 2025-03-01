04b855e9242b4373e1bbafd968016ade887bfa1c
Premysl Vlcek <premysl.vlcek@gmail.com>
2025-03-01 13:03:07 +0100

    Async delay fix

---
+++ Sources/SnapshotTesting/Extensions/Async+Delay.swift
@@ -0,0 +1,27 @@
+//
+//  Async+Delay.swift
+//  swift-snapshot-testing
+//
+//  Created by Premysl Vlcek on 01.03.2025.
+//
+
+import Foundation
+
+extension Async {
+  /// Delays Async `run` block
+  /// - Parameter timeInterval: Optional duration for Async to wait before running `run`
+  /// - Returns: Delayed `Async` unless no duration was provided then returns original `Async`
+  func delay(by timeInterval: TimeInterval?) -> Async<Value> {
+    guard let timeInterval = timeInterval else {
+      return self
+    }
+
+    return Async<Value> { callback in
+      run { value in
+        DispatchQueue.main.asyncAfter(deadline: .now() + timeInterval) {
+          callback(value)
+        }
+      }
+    }
+  }
+}
